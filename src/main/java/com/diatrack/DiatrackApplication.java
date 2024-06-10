c408c7f8e799f47c4f262764121914a23fca0d6f
kettaabii <elkettabimohammedamin@gmail.com>
2024-06-10 14:28:06 +0100

    first commit

---
+++ src/main/java/com/diatrack/DiatrackApplication.java
@@ -0,0 +1,13 @@
+package com.diatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiatrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diatrack/DiatrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiatrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
