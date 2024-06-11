6abe9bdaf4e0d59f0db001e0fddf259673053672
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-06-11 23:50:59 +0100

    j'ai cree l amethode add glycemie

---
+++ diabete/src/main/java/com/diabete/diabete/Repository/GlycemieRepository.java
@@ -0,0 +1,8 @@
+package com.diabete.diabete.Repository;
+
+import com.diabete.diabete.Models.Glycemie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GlycemieRepository extends JpaRepository<Glycemie,Integer> {
+
+}
