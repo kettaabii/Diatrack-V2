1c2aa6e48bf92c9104d247fbbf7aad03a721df8e
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-06-11 22:31:02 +0100

    j'ai modifiee dans les classes glecimie et user

--- diabete/src/main/java/com/diabete/diabete/Entities/Diabetiques.java
+++ diabete/src/main/java/com/diabete/diabete/Entities/Diabetiques.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 @NoArgsConstructor
 @Entity(name = "diabetiques")
 public class Diabetiques {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idDiabetiques;
@@ -36,60 +35,5 @@ public class Diabetiques {
     @OneToMany(mappedBy = "diabetiques")
     private Collection<Glycemie> valuesGlycemies;
 
-    public Integer getIdDiabetiques() {
-        return idDiabetiques;
-    }
-
-    public void setIdDiabetiques(Integer idDiabetiques) {
-        this.idDiabetiques = idDiabetiques;
-    }
-
-    public String getNameDiabetiques() {
-        return nameDiabetiques;
-    }
-
-    public void setNameDiabetiques(String nameDiabetiques) {
-        this.nameDiabetiques = nameDiabetiques;
-    }
-
-    public Integer getPoids() {
-        return poids;
-    }
-
-    public void setPoids(Integer poids) {
-        this.poids = poids;
-    }
-
-    public Integer getTaille() {
-        return taille;
-    }
-
-    public void setTaille(Integer taille) {
-        this.taille = taille;
-    }
-
-    public Integer getAgeDiabetiques() {
-        return ageDiabetiques;
-    }
-
-    public void setAgeDiabetiques(Integer ageDiabetiques) {
-        this.ageDiabetiques = ageDiabetiques;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Collection<Glycemie> getValuesGlycemies() {
-        return valuesGlycemies;
-    }
-
-    public void setValuesGlycemies(Collection<Glycemie> valuesGlycemies) {
-        this.valuesGlycemies = valuesGlycemies;
-    }
 
 }
