0a69d7bd2d9b723f881a01165090ee79e2cba2b4
kettaabii <elkettabimohammedamin@gmail.com>
2024-06-12 14:28:18 +0100

    commoit

--- diabete/src/main/java/com/diabete/diabete/Controllers/Recepies.java
+++ diabete/src/main/java/com/diabete/diabete/Controllers/Recepies.java
@@ -1,5 +1,6 @@
 package com.diabete.diabete.Controllers;
 
+import com.diabete.diabete.Models.Repas;
 import com.diabete.diabete.Services.RepasService;
 import com.diabete.diabete.Services.RepasServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class Recepies {
     @Autowired
     RepasService repasService;
     @GetMapping ("/Repas")
     public String Repas(Model model) {
-        
+        List<Repas> repas=repasService.getAllRepas();
+        model.addAttribute("listeRepas",repas );
 
     }
 
